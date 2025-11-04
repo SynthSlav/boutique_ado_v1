8969cb57c6d70aa771319ed244b333979d1d121b
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-04 13:32:04 +0100

    added sorting and product count page

--- products/views.py
+++ products/views.py
@@ -24,6 +24,9 @@ def all_products(request):
                 sortkey = "lower_name"
                 products = products.annotate(lower_name=Lower("name"))
 
+            if sortkey == "category":
+                sortkey = "category__name"
+
             if "direction" in request.GET:
                 direction = request.GET["direction"]
                 if direction == "desc":
@@ -44,13 +47,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    curent_sorting = f"{sort}_{direction}"
+    current_sorting = f"{sort}_{direction}"
 
     context = {
         "products": products,
         "search_term": query,
         "current_categories": categories,
-        "current_sorting": curent_sorting,
+        "current_sorting": current_sorting,
     }
 
     return render(request, "products/products.html", context)
